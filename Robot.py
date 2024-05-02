75cbaae2510865e6f20ef3a366a96921d5c051f3
SKamarry <stephaniekamarryas@gmail.com>
2024-05-02 16:31:07 -0300

    Create Robot Class

---
+++ Robot.py
@@ -0,0 +1,14 @@
+# Programa de Cálculo de Média
+
+
+def calcular_media(numeros):
+    return sum(numeros)/ len(numeros)
+
+
+# Lista de números
+numeros = [2,2,2,2,2]
+
+# Calculando a média
+media = calcular_media(numeros)
+
+print(f"A média é {media}")
